97de4cce81b9704e0876efc21b5184e5569bc67c
mdazharul869 <freelancermdazharul>
2023-12-21 23:15:20 +0600

    build

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from "@chakra-ui/theme-tools";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
   RouterProvider,
+  createBrowserRouter,
 } from "react-router-dom";
 import App from './App.jsx';
 import Home from './component/pages/Home/Home.jsx';
 import ErrorPage from './errorpage/ErrorPage.jsx';
 import './index.css';
+
 
 const router = createBrowserRouter ([
 
@@ -26,9 +28,28 @@ const router = createBrowserRouter ([
   },
 ])
 
+
+const styles = {
+	global: (props) => ({
+		body: {
+			
+			color: mode("#dddd")(props),
+		},
+	}),
+};
+
+const config = {
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config, styles });
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <ChakraProvider>
+     <ChakraProvider theme={theme}>
     <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
